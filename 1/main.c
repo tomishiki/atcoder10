ec9fb60cd85e3b440eef2bed3cb5c73c688f4846
tomishiki <tmyssy65@gmail.com>
2022-11-06 20:35:50 +0900

    solve 1

---
+++ 1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    if (a % 2 == 0 || b % 2 == 0) {
+        printf("Even\n");
+    } else {
+        printf("Odd\n");
+    }
+}
