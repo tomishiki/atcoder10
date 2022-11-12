02780ab790e2df19ba77895251f01ef0c4401721
tomishiki <tmyssy65@gmail.com>
2022-11-12 13:42:41 +0900

    solve 6

---
+++ 6/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+int compareInt(const void *a, const void *b) {
+    int aNum = *(int *) a;
+    int bNum = *(int *) b;
+
+    return bNum - aNum;
+}
+
+int main() {
+    int n, sumA = 0, sumB = 0, res;
+    scanf("%d", &n);
+    int array[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    qsort(array, SIZE_OF_ARRAY(array), sizeof(int), compareInt);
+
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            sumA += array[i];
+        } else {
+            sumB += array[i];
+        }
+    }
+
+    res = sumA - sumB;
+    printf("%d\n", res);
+}
